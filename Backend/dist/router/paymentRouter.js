6ebdd471d3f29e9a386f02662d1eacfb06085a85
ABHI-Theq <abhi03085e@gmail.com>
2025-09-03 09:56:55 +0530

    trigger comeplte codebase review

--- Backend/dist/router/paymentRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const paymentControllers_1 = require("../controllers/paymentControllers");
-const router = (0, express_1.Router)();
-router.route('/checkout').post(paymentControllers_1.checkout);
-router.route("/paymentverification").post(paymentControllers_1.paymentVerification);
-exports.default = router;
